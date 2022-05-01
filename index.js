88937f9acb51d8d412ac6d734441e174b912ce71
Serje <Serje.havandjian@gmail.com>
2022-05-01 18:52:48 -0400

    commit tree aaa527c876a757a4d589a4bb23b8e60b00eda055

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+
+const main = document.getElementById("main")
+main.remove()
+
+
+
+
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+
+newHeader.textContent = "YOUR-NAME is the champion"
+
+
+
+
+const grabBody = document.querySelector("body")
+
+const p = document.createElement("p")
+
+grabBody.append(p)
+p.append(newHeader)
